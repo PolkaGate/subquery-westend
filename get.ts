3264e9b566d7c8a2a9e02d121dc84cf55afae188
Nick-1979 <ekbatanifard@gmail.com>
2022-07-19 21:56:18 +0430

    Update get.ts

--- get.ts
+++ get.ts
@@ -1,8 +1,19 @@
 import request from 'umi-request';
 
-export async function query(): Promise<void> {
+export async function query(lost: string, rescuer: string): Promise<void> {
   // const query = 'query {transfers(first:2) {nodes {nodeId, id, from, to, amount, blockNumber}}}';
-  const query = 'query {recoveryVoucheds(first: 2) {nodes {nodeId, id, lost, rescuer, friend, blockNumber}}}';
+  const query = `query {
+    recoveryVoucheds (filter:
+     {lost:{equalTo:"${lost}"},
+      rescuer:{equalTo:"${rescuer}"}
+    }){ 
+      nodes 
+      {id,
+       blockNumber,
+       lost,
+       rescuer,
+       friend
+      }}}`;
   const res = await postReq(`https://api.subquery.network/sq/Nick-1979/polkagate-westend-queries`, { query });
 
   console.log('res:', res.data.recoveryVoucheds.nodes);
@@ -21,4 +32,4 @@ function getReq(api: string, data: Record<string, any> = {}, option?: Record<str
   });
 }
 
-query();
+query('5DoWzQ8PvjvcCSxiXc928T82EwfPzJAYA1eGRCno28RadQgP','5CG114jwh4CHMFsA9At6joNoLBz3hn3d479Y4KdrkBZXCS7w');
