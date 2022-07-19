77b810a389469710dbb5139519b9d338f5545631
Nick-1979 <ekbatanifard@gmail.com>
2022-07-19 23:53:24 +0430

    Update get.ts

--- get.ts
+++ get.ts
@@ -1,7 +1,6 @@
 import request from 'umi-request';
 
-export async function query(lost: string, rescuer: string): Promise<void> {
-  // const query = 'query {transfers(first:2) {nodes {nodeId, id, from, to, amount, blockNumber}}}';
+export async function getVouchers (lost: string, rescuer: string): Promise<void> {
   const query = `query {
     recoveryVoucheds (filter:
      {lost:{equalTo:"${lost}"},
@@ -14,22 +13,17 @@ export async function query(lost: string, rescuer: string): Promise<void> {
        rescuer,
        friend
       }}}`;
-  const res = await postReq(`https://api.subquery.network/sq/Nick-1979/polkagate-westend-queries`, { query });
+  const res = await postReq('https://api.subquery.network/sq/Nick-1979/westend', { query });
 
   console.log('res:', res.data.recoveryVoucheds.nodes);
 }
 
-function postReq(api: string, data: Record<string, any> = {}, option?: Record<string, any>): Promise<Record<string, any>> {
+function postReq (api: string, data: Record<string, unknown> = {}, option?: Record<string, unknown>): Promise<Record<string, any>> {
   return request.post(api, {
     data,
     ...option
   });
 }
-function getReq(api: string, data: Record<string, any> = {}, option?: Record<string, any>): Promise<Record<string, any>> {
-  return request.get(api, {
-    data,
-    ...option
-  });
-}
 
-query('5DoWzQ8PvjvcCSxiXc928T82EwfPzJAYA1eGRCno28RadQgP','5CG114jwh4CHMFsA9At6joNoLBz3hn3d479Y4KdrkBZXCS7w');
+// eslint-disable-next-line no-void
+void getVouchers('5DoWzQ8PvjvcCSxiXc928T82EwfPzJAYA1eGRCno28RadQgP', '5CG114jwh4CHMFsA9At6joNoLBz3hn3d479Y4KdrkBZXCS7w');
