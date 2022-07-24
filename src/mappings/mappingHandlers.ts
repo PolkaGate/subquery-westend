49b1f9c94250ef434bb1a7e1ff3473d6238eb38d
Kami <ekbatanifard@gmail.com>
2022-07-24 14:29:50 +0430

    add RecoveryInitiated

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,10 +1,9 @@
 import { SubstrateEvent } from "@subql/types";
-import { RecoveryVouched } from "../types";
+import { RecoveryVouched, RecoveryInitiated } from "../types";
 
 export async function handleVouches(event: SubstrateEvent): Promise<void> {
   // Get data from the event
   // RecoveryVouched(AccountId32, AccountId32, AccountId32)
-  logger.info(JSON.stringify(event));
   const lost = event.event.data[0];
   const rescuer = event.event.data[1];
   const friend = event.event.data[2];
@@ -18,4 +17,19 @@ export async function handleVouches(event: SubstrateEvent): Promise<void> {
   vouch.rescuer = rescuer.toString();
   vouch.friend = friend.toString();
   await vouch.save();
+}
+
+export async function handleInitiations(event: SubstrateEvent): Promise<void> {
+  // RecoveryInitiated(AccountId32, AccountId32)
+  const lost = event.event.data[0];
+  const rescuer = event.event.data[1];
+
+  // Create the new initiation entity
+  const initiation = new RecoveryInitiated(
+    `${event.block.block.header.number.toNumber()}-${event.idx}`
+  );
+  initiation.blockNumber = event.block.block.header.number.toBigInt();
+  initiation.lost = lost.toString();
+  initiation.rescuer = rescuer.toString();
+  await initiation.save();
 }
